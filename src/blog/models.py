b197e76f10ffa46b568cd1efee681b267e0db426
Thedatababbler <andy198318@aol.com>
2020-08-09 07:46:31 -0700

    "first commit"

---
+++ src/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+# creat fields to database
+class BlogPost(models.Model):
+    title = models.TextField()
+    content = models.TextField(null=True, blank=True)
+
+# run manage.py makemigrations and manage.py migrate everytime
+# you change the models
